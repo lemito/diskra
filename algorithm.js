79a293216d6ffcc4cccf2cef6e0ddad5def767cb
Michael lemito <delovoy.lemit@gmail.com>
2024-05-15 16:52:56 +0300

    Super-duper код

--- algorithm.js
+++ algorithm.js
@@ -14,20 +14,20 @@ function myAlgoritm(inputStr) {
       A[i - 1] = strings[i].split(/\s+/);
     }
     // 1 строка с числом cjunt+ count строк в матрице смежности
-    if (strings[count + 1] == "<Text>") {
+    if (strings[count + 1] === "<Text>") {
       do {
         text[iT] = strings[iT + count + 2];
         iT++;
       } while (
         count + iT < strings.length &&
-        strings[count + iT + 2][0] != "<"
+        strings[count + iT + 2][0] !== "<"
       );
     } else {
       msg += " Нет секции <Text>.";
     }
     iT = iT ? iT + 1 : 0;
     // iT строк в секции <Text> + 1 строка с названием <Text>
-    if (strings[count + iT + 1] == "<Vertexes>") {
+    if (strings[count + iT + 1] === "<Vertexes>") {
       for (let i = 1; i < count + 1; i++) {
         vertexes[i - 1] = strings[i + count + 1 + iT].split(/\s+/);
       }
@@ -38,7 +38,7 @@ function myAlgoritm(inputStr) {
     }
     //считаем, что если секция<Vertexes> есть, то в ней count строк
     //debugger;
-    if (strings[count + 1 + iV + iT] == "<Edges>") {
+    if (strings[count + 1 + iV + iT] === "<Edges>") {
       let m = strings.length - count - 2 - iV - iT;
       for (let i = 1; i < m; i++) {
         edges[i - 1] = strings[i + count + iV + iT + 1].split(/\s+/);
@@ -51,55 +51,75 @@ function myAlgoritm(inputStr) {
     rez.innerHTML += " Готово. <hr>";
     // обрабатываем
        /* МОЙ КОД */
+    console.log(A, count);
 
-    // массив посещенных вершнин; изначально никто не посещен
-    let visited = new Array(count).fill(false);
-    // глубина; изначально глубина всех точек = 0
-    let depth = new Array(count).fill(0);
-    // массив с родительскими вершинами для каждой вершинки; при отсутствии == -1; 
-    let parent = new Array(count).fill(-1);
-    // шаг алгоритма; мы еще никуда не шли, так что шаг = 0
-    let time = 0;
+    // в файле поступает матрица из строк, переделаем их в number
+    function convertMatrixToStringInt(matrix) {
+        // Преобразование каждого элемента строки в число
+        return matrix.map(row => row.map(cell => parseInt(cell)));
+    }
+    A = convertMatrixToStringInt(A);
+    console.log(A);
 
-    /**
-     * @brief поиск в глубину
-     **/
-    function dfs(v) {
-      visited[v] = true;
-      time++;
-      depth[v] = time;
-      for (let i = 0; i < count; i++) {
-        if (A[v][i] !== "0" && !visited[i]) {
-          parent[i] = v;
-          dfs(i);
+    function findArticulationPoints(matrix, N) {
+        let visited = new Array(N).fill(false);
+        let depth = new Array(N).fill(-1);
+        let low = new Array(N).fill(-1);
+        let time = 0;
+        let articulationPoints = [];
+
+        function DFS(v, parent) {
+            visited[v] = true;
+            depth[v] = low[v] = ++time;
+
+            let children = 0;
+            let isArticulation = false;
+
+            for (let i = 0; i < N; i++) {
+                if (matrix[v][i] === 1) {
+                    if (!visited[i]) {
+                        children++;
+                        DFS(i, v);
+
+                        low[v] = Math.min(low[v], low[i]);
+
+                        if (parent!== -1 && low[i] >= depth[v]) {
+                            isArticulation = true;
+                        }
+                    } else if (i!== parent) {
+                        low[v] = Math.min(low[v], depth[i]);
+                    }
+                }
+            }
+
+            if ((children > 1) || (isArticulation)) {
+                articulationPoints.push(v+1);
+            }
         }
-      }
+
+        for (let i = 0; i < N; i++) {
+            if (!visited[i]) {
+                DFS(i, -1);
+            }
+        }
+
+        return articulationPoints;
     }
 
-    // Начало обхода с вершины 0
-    dfs(0);
-
-    // массив шарниров
-    let articulationPoints = [];
-    for (let v = 0; v < count; v++) {
-      let children = 0;
-      let descendants = 0;
-      for (let i = 0; i < count; i++) {
-        if (A[v][i] !== "0" && parent[v] !== i) {
-          children++;
-          if (depth[i] > depth[v]) {
-            descendants++;
-          }
-        }
-      }
-      if (children > 1 || (children === 1 && descendants > 0)) {
-        articulationPoints.push(v);
-      }
+    console.log(typeof A[0][0])
+    const articulationPoints = findArticulationPoints(A, count);
+    console.log(articulationPoints);
+    if (articulationPoints.length !== 0) {
+        rez.innerHTML += "Шарниры графа: " + articulationPoints.join(', ');
+        outputStr = "Шарниры графа: " + articulationPoints.join(', ');
+    }
+    else {
+        rez.innerHTML += "Шарниры в графе отсуствуют!";
+        outputStr = "Шарниры в графе отсуствуют!";
+        rez.style.color = "red";
     }
 
-    // Вывод результатов
-    rez.innerHTML += "<br>";
-    rez.innerHTML += articulationPoints;
-    console.log("Шарниры графа:", articulationPoints);
+
     return outputStr;
+
   }
